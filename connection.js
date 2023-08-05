855fc992a28a0c3eb4b93324d2b0eb2f516491ce
Akindejoye <tydruhill@gmail.com>
2023-08-05 05:06:30 +0100

    Connection to database

---
+++ connection.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+const connectionStr = process.env.MONGO_URL;
+
+mongoose.connect(connectionStr, {useNewUrlParser: true})
+  .then(() => console.log('connected to database'))
+  .catch(err => console.log(err));
+
+mongoose.connection.on('error', err => {
+  console.log(err);
+});
