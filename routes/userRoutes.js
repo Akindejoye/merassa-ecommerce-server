dde7dbbcca801a23d147a2b3c501190416a40f6e
Akindejoye <tydruhill@gmail.com>
2023-08-10 18:15:02 +0100

    Get users controller

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -18,14 +18,25 @@ router.post('/signup', async(req, res) => {
 
 // login
 
- router.post('/login', async(req, res) => {
-  const { email, password } = req.body;
+router.post('/login', async(req, res) => {
+const { email, password } = req.body;
+try {
+  const user = await User.findByCredentials(email, password);
+  res.json(user);
+} catch (error) {
+  res.status(400).send(error.message);
+}
+});
+
+// get users
+
+router.get('/', async(req, res) => {
   try {
-    const user = await User.findByCredentials(email, password);
-    res.json(user);
+    const users = await User.find({ isAdmin: false }).populate('orders');
+    res.json(users);
   } catch (error) {
     res.status(400).send(error.message);
   }
- })
+})
 
 module.exports = router;
