e64a578945349eb5354146c3029d8a5b9e532a72
Akindejoye <tydruhill@gmail.com>
2023-09-18 15:37:33 +0100

    Image Routes

--- server.js
+++ server.js
@@ -13,12 +13,14 @@ const io = new Server(server, {
 const User  = require('./models/User');
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
+const imageRoutes = require('./routes/imageRoutes');
 
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/users', userRoutes);
+app.use('/images', imageRoutes);
 
 server.listen(8080, () => {
   console.log('Server running on port', 8080);
